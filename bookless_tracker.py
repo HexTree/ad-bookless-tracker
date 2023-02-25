45f7607de06e671d5851dfefc731ebd1a4f9e714
HexTree <>
2023-02-25 19:02:26 +0530

    add button to clear all checkboxes, and cleaner method for updating display

--- bookless_tracker.py
+++ bookless_tracker.py
@@ -3,9 +3,8 @@ import sys
 from tkinter import *
 from PIL import ImageTk, Image, ImageDraw, ImageFont
 
-checkboxes = {}
-button_commands = {}
-button_vars = {}
+icon_vars = {}
+buildings_vars = {}
 labels = {}
 buildings_count = 1
 
@@ -24,34 +23,18 @@ def resource_path(relative_path):
 class Checkbar(Frame):
     def __init__(self, parent=None, picks=[], side=LEFT, anchor=W):
         Frame.__init__(self, parent)
-        self.vars = []
         for pick in picks:
-            var = button_vars[pick.lower()] if pick.lower() in button_vars else IntVar()
+            pick_name = pick.lower()
+            if pick_name in icon_vars:
+                var = icon_vars[pick_name]
+            else:
+                var = IntVar()
+                if pick.lower() != "monster hut 3":
+                    buildings_vars[pick_name] = var
 
-            def icon_toggle(icon_name=pick.lower()):
-                if button_vars[icon_name].get():
-                    x, y = icon_positions[icon_name]
-                    labels[icon_name].place(x=x, y=y)
-                else:
-                    labels[icon_name].place_forget()
-
-            def building_toggle(building_name=pick.lower(), building_var=var):
-                global buildings_count
-                if building_name == "monster hut 3":
-                    return
-                if building_var.get():
-                    buildings_count += 1
-                else:
-                    buildings_count -= 1
-                name_or_count_changed(None)
-
-            chk = Checkbutton(self, text=pick, variable=var, command=icon_toggle if pick.lower() in labels else building_toggle)
+            chk = Checkbutton(self, text=pick, variable=var, command=update_display)
             chk.configure(background='#D39B6A', activebackground='#D39B6A')
             chk.pack(side=side, anchor=anchor, expand=YES)
-            self.vars.append(var)
-
-    def state(self):
-        return map((lambda var: var.get()), self.vars)
 
 
 if __name__ == '__main__':
@@ -93,7 +76,31 @@ if __name__ == '__main__':
         labels[icon] = label
 
         icon_var = IntVar()
-        button_vars[icon] = icon_var
+        icon_vars[icon] = icon_var
+
+    def update_display(event=None):
+        global save_label, save_photo, buildings_count
+        # update buildings count
+        buildings_count = 1
+        for var in buildings_vars.values():
+            buildings_count += var.get()
+
+        # create updated image with new name and building count
+        save_screen = Image.open(resource_path("data/save_screen.png"))
+        draw = ImageDraw.Draw(save_screen)
+        azure_font = ImageFont.truetype(resource_path('data/Azure_Dreams.ttf'), 30)
+        draw.text((130, 19), name_var.get()[:15], font=azure_font, fill='black')
+        draw.text((464, 83), str(buildings_count), font=azure_font, fill='black')
+        save_photo = ImageTk.PhotoImage(save_screen, master=graphic)
+        save_label.configure(image=save_photo)
+
+        # enable/disable icon labels
+        for icon, var in icon_vars.items():
+            label = labels[icon]
+            if var.get():
+                label.place(x=icon_positions[icon][0], y=icon_positions[icon][1])
+            else:
+                label.place_forget()
 
     # Create checkbuttons linked to images
     create_checklist('Girls', ['Nico', 'Selfi', 'Fur', 'Patty', 'Vivian', 'Mia', 'Cherrl'])
@@ -113,29 +120,17 @@ if __name__ == '__main__':
     name_entry = Entry(root, textvariable=name_var, font=('calibre', 10, 'normal'))
     name_entry.grid(row=row, sticky=W, padx=4, pady=4)
 
-    def name_or_count_changed(event):
-        global save_label, save_photo
-        save_screen = Image.open(resource_path("data/save_screen.png"))
-        draw = ImageDraw.Draw(save_screen)
-        azure_font = ImageFont.truetype(resource_path('data/Azure_Dreams.ttf'), 30)
-        draw.text((130, 19), name_var.get()[:15], font=azure_font, fill='black')
-        draw.text((464, 83), str(buildings_count), font=azure_font, fill='black')
-        save_photo = ImageTk.PhotoImage(save_screen, master=graphic)
-        save_label.configure(image=save_photo)
-    name_or_count_changed(None)
+    update_display()
+    root.bind('<Return>', update_display)
 
-    root.bind('<Return>', name_or_count_changed)
-
-    # def reset():
-    #     for widget in root.winfo_children():
-    #         if isinstance(widget, Checkbar):
-    #             for w in widget.winfo_children():
-    #                 if isinstance(w, Checkbutton):
-    #                     w.deselect()
-    # Button(root, text='Clear', command=reset).grid(row=row, sticky=E, padx=4, pady=4)
+    def clear():
+        for widget in root.winfo_children():
+            if isinstance(widget, Checkbar):
+                for w in widget.winfo_children():
+                    if isinstance(w, Checkbutton):
+                        w.deselect()
+        update_display()
+    Button(root, text='Clear', command=clear).grid(row=row, sticky=E, padx=4, pady=4)
 
     # run
     root.mainloop()
-
-
-
